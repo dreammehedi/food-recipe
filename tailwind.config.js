d050e7e0e394d8ea1246b02641a83d19460fc0e5
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-03-15 20:10:33 +0600

    Favicon, Font-Family, Container And Color Are Added.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      "fira-sans": ["Fira Sans", "sans-serif"],
+      Lexend: ["Lexend", "sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "1.5rem",
+    },
+    extend: {
+      colors: {
+        primary: "#0BE58A",
+        dark: "#150B2B",
+        "dark-70": "#150B2BB3",
+        "dark-60": "#150B2B99",
+        "dark-5": "#150B2B0D",
+        dark2: "#282828",
+        "dark-06": "#878787",
+      },
+    },
   },
   plugins: [],
 };
