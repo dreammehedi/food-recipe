69cb2c9c0a18a7e43c840e4f88696ffcc862ded2
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-03-17 10:39:02 +0600

    Preloader Pblm Tragel

--- src/components/preloader/Preloader.jsx
+++ src/components/preloader/Preloader.jsx
@@ -15,15 +15,11 @@ function Preloader() {
   return (
     <div>
       {loading ? (
-        <div className="w-full h-screen  cursor-wait flex justify-center items-center fixed top-1/2 -translate-x-1/2 -translate-y-1/2  left-1/2 bg-white z-[999999999999]">
-          {/* <BallTriangle
-            height={100}
-            width={100}
-            radius={5}
-            color="#0BE58A"
-            ariaLabel="food recipe loading"
-            visible={true}
-          /> */}
+        <div
+          className={`${
+            loading ? "block" : "hidden"
+          } w-full h-screen flex justify-center items-center fixed top-1/2 -translate-x-1/2 -translate-y-1/2  left-1/2 bg-white z-[999999999999]`}
+        >
           <div className="flex justify-center items-center h-screen">
             <div className="rounded-full h-20 w-20 bg-primary animate-ping"></div>
           </div>
